ef0075484cb9d3767e45d8779b34002439d67bb3
Khanyiso Mapuma <mapumak@gmail.com>
2024-07-23 04:35:27 +0200

    Add tv shows list.
    The purpose of this commit is to add a movie list screen with dummy data. A sign in redirect has also been added, with no condition and validation as of yet

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,16 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Landing from "./pages/landing";
+import ShowsList from "./pages/shows";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" Component={Landing} />
+        <Route path="/shows" Component={ShowsList} />
       </Routes>
     </Router>
   );
 }
-
 export default App;
---
+++ src/pages/shows/index.tsx
@@ -0,0 +1,77 @@
+import React from "react";
+import { Space, Table, Tag } from "antd";
+import VisibilityOutlinedIcon from "@mui/icons-material/VisibilityOutlined";
+import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlined";
+import { Card, CardContent, CardHeader, Grid } from "@mui/material";
+
+const ShowsList = () => {
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+      render: (text: any) => <a>{text}</a>,
+    },
+    {
+      title: "Description",
+      dataIndex: "description",
+      key: "description",
+    },
+    {
+      title: "Tags",
+      key: "tags",
+      dataIndex: "tags",
+      render: (_: any, { tags }: any) => (
+        <>
+          {tags.map((tag: string) => {
+            return <Tag key={tag}>{tag.toUpperCase()}</Tag>;
+          })}
+        </>
+      ),
+    },
+    {
+      title: "Action",
+      key: "action",
+      render: (_: any, record: any) => (
+        <Space size="middle">
+          <VisibilityOutlinedIcon />
+          <DeleteOutlinedIcon />
+        </Space>
+      ),
+    },
+  ];
+  const dummyData = [
+    {
+      key: "1",
+      name: "One Piece",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus vel metus et maximus.",
+      tags: ["nice", "developer"],
+    },
+    {
+      key: "2",
+      name: "Stranger Things",
+      description:
+        "Sed eget malesuada nibh. Nam gravida a odio sit amet feugiat. Vivamus eu porta metus. Aliquam massa metus, feugiat id purus eu, lacinia condimentum eros",
+      tags: ["loser"],
+    },
+    {
+      key: "3",
+      name: "Peaky Blinders",
+      description:
+        "Aliquam eu aliquam mauris, et pulvinar eros. Cras vitae placerat ipsum",
+      tags: ["cool", "teacher"],
+    },
+  ];
+  return (
+    <Grid container={true} item={true} xs={12}>
+      <Card style={{ width: "100%" }}>
+        <CardHeader title="TV Shows List"></CardHeader>
+        <CardContent>
+          <Table columns={columns} dataSource={dummyData} />
+        </CardContent>
+      </Card>
+    </Grid>
+  );
+};
+export default ShowsList;
